1a477d9318eb8c534e0eb3ebc0b609aeb1417828
albanvn <albanvn@gmail.com>
2013-10-03 17:43:36 +0200

    Add Changelog.txt and message if last sentence empty

--- repeat2.js
+++ repeat2.js
@@ -1,3 +1,5 @@
+const gs_msg_forgetit="je ne me souviens plus de ce que j'ai dit|je ne me rappelle plus de ce que j'ai dit";
+
 exports.init = function(SARAH)
 {
 }
@@ -6,5 +8,7 @@ exports.action = function(data, callback, config, SARAH)
 {
 	if (SARAH.context.lastspeak!=undefined)
 	  SARAH.speak(SARAH.context.lastspeak);
+	else
+	  SARAH.speakR(gs_msg_forgetit);
 	callback();
 }
